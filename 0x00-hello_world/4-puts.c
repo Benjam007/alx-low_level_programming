bf0fabc1d73adc69a1017dd2162e96e6985c8b47
Benjam007 <clementbenjamin07@gmail.com>
2022-12-14 06:14:03 -0800

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
-/*
- * main - entry point of the programme
- * Return: returns 0
+/**
+ * main - main block
+ * Return: returns  0
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
