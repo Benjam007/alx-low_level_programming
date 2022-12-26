7f8d9aa0dadd21ecd0409d4b02b026f0e75e8322
Benjam007 <clementbenjamin07@gmail.com>
2022-12-26 10:58:06 -0800

    change

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,13 +20,14 @@ int main(void)
 	{
 		printf("is positive\n");
 	}
-	else if (n = 0)
+	else if (n == 0)
 	{
 		printf("is zero\n");
 	}
 	else 
 	{
-		print("is negative\n");
+		printf("is negative\n");
+	}
 
 	return (0);
 }
