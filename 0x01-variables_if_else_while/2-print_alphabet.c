fdeb357b0972b11ec56393811bba425dfc2f4dff
Benjam007 <clementbenjamin07@gmail.com>
2022-12-26 14:39:37 -0800

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,12 +8,9 @@
 
 int main(void)
 {
-    char letters;
-
-    for (letters = 'a'; letters <= 'z'; letters++) 
-    
-    putchar(letters);
-    putchar('\n');
-    
-    return (0);
+char letters;
+for (letters = 'a'; letters <= 'z'; letters++)
+putchar(letters);
+putchar('\n');
+return (0);
 }
