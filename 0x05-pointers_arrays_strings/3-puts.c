bc2c33b4acca1c13883243506f7f6d5101fbb024
Benjam007 <clementbenjamin07@gmail.com>
2023-01-12 03:53:02 -0800

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts - a function that prints a string, 
+ * _puts - a function that prints a string,
  * followed by a new line, to stdout
  * @str: parameter
  */
@@ -9,6 +9,7 @@
 void _puts(char *str)
 {
 	int i;
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
