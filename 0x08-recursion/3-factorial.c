adcbdb1341fb01ea67ea4eb43b2b22e181b56895
Benjam007 <clementbenjamin07@gmail.com>
2023-01-16 15:23:43 -0800

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,17 +8,12 @@
 
 int factorial(int n)
 {
-	int *r = n;
+	int r = n;
 
 	if (n < 0)
-	{
 		return (-1);
-	}
-	else if (n > 0 && n < 2)
-	{
-	return (1);
-		r = n * (n-1);
-		n--;
-	}
+	else if (n >= 0 && n <= 1)
+		return (1);
+	r *= factorial(n - 1);
+	return (r);
 }
-
