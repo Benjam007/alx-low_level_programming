c6c1bd1d817b12a8e8fe12e28b1cdb905efc43e8
Benjam007 <clementbenjamin07@gmail.com>
2023-01-27 17:19:19 -0800

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,38 +1,37 @@
-xbase01
-/
-alx-low_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-alx-low_level_programming/0x0B-malloc_free/0-create_array.c
-@xbase01
-xbase01 creat array
- 1 contributor
-22 lines (21 sloc)  435 Bytes
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 /**
- * create_array - creates array and initialize it
- * @size: size of array
- * @c: character
- * Return: returns a pointer if successful or NULL otherwise
+ * create_array - creates an array of chars dynamically
+ * @size: size of the array
+ * @c: the string
+ * Return: 0 means success except defined otherwise
  */
 char *create_array(unsigned int size, char c)
 {
-unsigned int i;
-char *ptr;
+	char *buffer;
+	unsigned int position;
 
-if (size == 0)
-return (NULL);
-ptr = (char *) malloc(size * sizeof(char));
-if (ptr == NULL)
-return (NULL);
-for (i = 0; i < size; i++)
-ptr[i] = c;
-return (ptr);
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	/*Define values with malloc*/
+	buffer = (char *) malloc(size * sizeof(c));
+
+	if (buffer == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		position = 0;
+		while (position < size) /*While for array*/
+		{
+			*(buffer + position) = c;
+			position++;
+		}
+
+		return (buffer);
+	}
 }
